38629424f8b39bca0b5774ac3206d914337e941b
bluebronze <2013novaknadya@gmail.com>
2022-11-12 16:16:53 -0500

    Created Driver outline and method stubs

--- src/temp/FantasyDraft.java
+++ src/temp/FantasyDraft.java
@@ -4,7 +4,57 @@ public class FantasyDraft {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
 
 	}
 
+	//METHODS BEGIN HERE
+	public static void oDraft(String playerName, char leagueMember) {
+		// Draft player to league member
+		
+	}
+	
+	public static void iDraft(String playerName) {
+		// Draft player to controling league member
+		
+	}
+	
+	public static void overall(String position) {
+		//print player rankings in given position
+	}
+	
+	public static void pOverall () {
+		//Overall just for pitchers
+	}
+	
+	public static void team (char leagueMember) {
+		//print roster for given member
+	}
+	
+	public static void stars() {
+		//Same as TEAM, but the ordering of the players matches the order in which they were drafted overall
+	}
+	
+	public static void save (String filename) {
+		
+	}
+	
+	public static void quit() {
+		
+	}
+	
+	public static void restore(String filename) {
+		
+	}
+	
+	public static void evalFun() {
+		
+	}
+	
+	public static void help () {
+		//list commands/syntax
+	}
+	
 }
