ae3935e10d3da474501315beebeb39f5efe0e4ba
bluebronze <2013novaknadya@gmail.com>
2022-11-12 20:14:03 -0500

    Added FantasyTeam stubs

--- src/temp/FantasyTeam.java
+++ src/temp/FantasyTeam.java
@@ -2,4 +2,27 @@ package temp;
 
 public class FantasyTeam {
 
+	
+	public static void draft(String playerName) {
+		// Draft player to controling league member
+		
+	}
+	
+	public static void remove() {
+		
+	}
+	
+	public static boolean onTeam() {
+		// Fill in
+		
+		boolean present = false;
+		
+		return present;
+	}
+	
+	public String toString() {
+		
+		return "";
+	}
 }
+
